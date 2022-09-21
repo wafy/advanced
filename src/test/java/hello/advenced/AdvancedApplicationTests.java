bd0dfe7e2b1d5f429c93d6bb8a363a5d15a7f9ae
최선혁 <wafy75@gmail.com>
2022-09-21 14:11:58 +0900

    빈 프로젝트 최초 커밋

---
+++ src/test/java/hello/advenced/AdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.advenced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
