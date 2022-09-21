a796485c344ecd5e1071aab974f0949a484fbb4f
최선혁 <wafy75@gmail.com>
2022-09-21 17:28:36 +0900

    오타 수정

--- src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -20,8 +20,8 @@ public class FieldServiceTest {
         threadB.setName("thread-B");
 
         threadA.start();
-//        sleep(2000); //동기성 문제가 발생X
-        sleep(100); //동기성 문제가 발생O
+//        sleep(2000); //동시성 문제가 발생X
+        sleep(100); //동시성 문제가 발생O
         threadB.start();
 
         sleep(3000); //메인 쓰레드 종료 대기
