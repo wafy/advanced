f4ed0e38c9c33f4504fb888be38ce8c4df360380
최선혁 <wafy75@gmail.com>
2022-09-26 12:05:21 +0900

    템플릿메서드 패턴 예졔2

---
+++ src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,19 @@
+package hello.advanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        call(); //상속
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTimme = endTime - startTime;
+        System.out.println("resultTimme = " + resultTimme);
+    }
+
+    protected abstract void call();
+}
